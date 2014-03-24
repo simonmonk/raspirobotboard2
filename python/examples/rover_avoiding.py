8b247f6b29ca5e76a6c752beaeaa994acd80598e
Simon Monk <srmonk@gmail.com>
2014-03-24 17:23:17 +0000

    Update rover_avoiding.py

--- python/examples/rover_avoiding.py
+++ python/examples/rover_avoiding.py
@@ -1,5 +1,5 @@
+# Attach: SR-04 Range finder, switch on SW1, and of course motors.
 
-# Attach: SR-04 Range finder and of course motors.
 
 
 from rrb2 import *
@@ -7,14 +7,25 @@ import time, random
 
 rr = RRB2()
 
+# if you dont have a switch, change the value below to True
+running = False
+
 def turn_randomly():
     turn_time = random.randint(1, 3)
-    rr.left(turn_time, 0.5)
+    if random.randint(1, 2) == 1:
+        rr.left(turn_time, 1.0)
+    else:
+        rr.right(turn_time, 1.0)
     rr.stop()
 
 while True:
     distance = rr.get_distance()
-    if distance < 30:
+    if distance < 50 and running:
         turn_randomly()
-    rr.forward(0, 0.7)
-    time.sleep(0.5)
+    if running:
+        rr.forward(0, 1.0)
+    if rr.sw2_closed():
+        running = not running
+    if not running:
+        rr.stop()
+    time.sleep(0.2)
